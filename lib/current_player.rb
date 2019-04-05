9a085b83a928f328d6224314c2a35630466f8e10
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-05 20:06:16 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   turns = 0
-  board.each do |turns|
-    if turns == "X" || turns == "O"
+  board.each do |le|
+    if le == "X" || le == "O"
       turns +=1 
     end
   end
