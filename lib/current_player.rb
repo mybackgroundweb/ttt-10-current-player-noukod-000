deada69cfe4e56d5b5cb178a6a7b35b6b1366c51
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-05 19:55:20 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,11 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 def turn_count(board)
-  if (board.X && board.O)
+  if (if board)
     return board.size
   end
 end
 
 def current_player
+  
 end
